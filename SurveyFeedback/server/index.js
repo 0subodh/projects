2a359370e7ded2ab0c5cc9bf6cda96a855ce8096
0subodh <subodh1adhikari@gmail.com>
2024-02-25 15:56:23 +0545

    implementing passport and googleStrategy 01

--- SurveyFeedback/server/index.js
+++ SurveyFeedback/server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+authRoutes(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
